787a63075d5e782c028230179a332fa9c306c15b
Alan Henry <alandavidhenry@gmail.com>
2024-09-30 14:50:47 +0100

    fix: add cert and port env vars

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,12 +5,19 @@ const db = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MY_SQL_DATABASE,
+  port: parseInt(process.env.MYSQL_PORT || '3306'),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ...(process.env.NODE_ENV === 'production'
+    ? {
+        ssl: {
+          rejectUnauthorized: true,
+          ca: process.env.MYSQL_SSL_CA
+        }
+      }
+    : {})
 })
 
 export default db
+
